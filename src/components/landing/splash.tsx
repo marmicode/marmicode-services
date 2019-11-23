db44461a3c9017e62d77f66c7c8a0b9e47d9a64a
Younes Jaaidi <yjaaidi@gmail.com>
2019-11-23 12:13:02 +0100

    🛠 move splash to dedicated component

---
+++ src/components/landing/splash.tsx
@@ -0,0 +1,48 @@
+import '@emotion/core'
+import { graphql, useStaticQuery } from 'gatsby'
+import { Parallax } from 'react-parallax'
+import {
+  hero,
+  marmicodeColor,
+  marmicodeColorWithOpacity,
+} from '../../config/config'
+import { createGradient } from '../../helpers/create-gradient'
+import { Hero } from '../hero'
+
+export const Splash = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      background: file(relativePath: { eq: "cooking-pot.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1000) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  const gradient = {
+    background: [
+      marmicodeColor,
+      ...createGradient({
+        colorA: marmicodeColorWithOpacity(0.9),
+        colorB: marmicodeColor,
+      }),
+    ],
+  }
+
+  return (
+    <Parallax
+      bgImage={data.background.childImageSharp.fluid.src}
+      bgImageAlt="marmicode cooking pot"
+      bgImageStyle={{ height: '170%', objectFit: 'cover', opacity: 0.65 }}
+      css={{ height: 'calc(100vh - 60px)', ...gradient }}
+      strength={400}
+    >
+      <div css={{ marginTop: '30vh' }}>
+        <Hero title={hero.title} subtitle={hero.subtitle} />
+      </div>
+    </Parallax>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,10 @@
 import '@emotion/core'
-import { graphql, useStaticQuery } from 'gatsby'
 import React from 'react'
-import { Parallax } from 'react-parallax'
-import { Hero } from '../components/hero'
+import { Splash } from '../components/landing/splash'
 import { Layout } from '../components/layout'
 import { Slant } from '../components/slant'
-import {
-  hero,
-  marmicodeColor,
-  marmicodeColorWithOpacity,
-} from '../config/config'
-import { createGradient } from '../helpers/create-gradient'
 
 export const IndexPage = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      background: file(relativePath: { eq: "cooking-pot.jpg" }) {
-        childImageSharp {
-          fluid(maxWidth: 1000) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-    }
-  `)
-
-  const gradient = {
-    background: [
-      marmicodeColor,
-      ...createGradient({
-        colorA: marmicodeColorWithOpacity(0.9),
-        colorB: marmicodeColor,
-      }),
-    ],
-  }
-
   const sectionList = [
     {
       title: 'Services',
@@ -55,17 +25,7 @@ export const IndexPage = () => {
 
   return (
     <Layout title={'Welcome'}>
-      <Parallax
-        bgImage={data.background.childImageSharp.fluid.src}
-        bgImageAlt="marmicode cooking pot"
-        bgImageStyle={{ height: '170%', objectFit: 'cover', opacity: 0.65 }}
-        css={{ height: 'calc(100vh - 60px)', ...gradient }}
-        strength={400}
-      >
-        <div css={{ marginTop: '30vh' }}>
-          <Hero title={hero.title} subtitle={hero.subtitle} />
-        </div>
-      </Parallax>
+      <Splash />
       <Slant />
       {sectionList.map(section => (
         <section
