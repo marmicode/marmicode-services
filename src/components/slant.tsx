0734aff503bb55801480302341c28c9c6ffc4f70
Younes Jaaidi <yjaaidi@gmail.com>
2019-11-23 13:10:28 +0100

    🚧 add sections

--- src/components/slant.tsx
+++ src/components/slant.tsx
@@ -1,27 +1,21 @@
 import { css } from '@emotion/core'
 
-const slant = ({ height, isTop }: { height: number; isTop: boolean }) => {
+const slant = ({ height }: { height: number }) => {
   return css({
     backgroundColor: '#fff',
     transform: 'rotate(-2deg)',
     overflow: 'hidden',
-    height: `${height}px`,
+    height: `${height / 2}px`,
     width: '102%',
     position: 'absolute',
-    bottom: `-${height / 2}px`,
+    bottom: `${height / 4}px`,
   })
 }
 
-const slantContainer = ({
-  height,
-  isTop,
-}: {
-  height: number
-  isTop: boolean
-}) => {
+const slantContainer = ({ height }: { height: number }) => {
   return css({
     position: 'absolute',
-    bottom: 0,
+    bottom: `-${height / 2}px`,
     overflow: 'hidden',
     height: `${height}px`,
     width: '100%',
@@ -29,11 +23,11 @@ const slantContainer = ({
   })
 }
 
-export const Slant = ({ isTop }: { isTop?: boolean }) => {
-  const height = 80
+export const Slant = () => {
+  const height = 160
   return (
-    <div css={slantContainer({ height, isTop })}>
-      <div css={slant({ height, isTop })}></div>
+    <div css={slantContainer({ height })}>
+      <div css={slant({ height })}></div>
     </div>
   )
 }
