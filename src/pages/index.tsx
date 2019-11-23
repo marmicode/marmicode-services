edbc38d5a430d76f5d1ba4f0d6f2d636cfb5eb45
Younes Jaaidi <yjaaidi@gmail.com>
2019-11-23 12:27:04 +0100

    🚧 add sections

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,40 @@ import { Splash } from '../components/landing/splash'
 import { Layout } from '../components/layout'
 import { Slant } from '../components/slant'
 
+export interface SectionData {
+  title: string
+}
+
+export const SectionTitle = ({ title }: { title: string }) => {
+  return (
+    <h1
+      css={{
+        color: '#444',
+        fontFamily: '"Source Sans Pro", Arial, sans-serif',
+        fontSize: '30px',
+        fontWeight: 300,
+        textTransform: 'uppercase',
+        '@media screen and (min-width: 768px)': {
+          fontSize: '50px',
+        },
+      }}
+    >
+      {title}
+    </h1>
+  )
+}
+
+export const Section = ({ section }: { section: SectionData }) => {
+  return (
+    <>
+      <section css={{ textAlign: 'center', height: '100vh' }}>
+        <SectionTitle title={section.title} />
+      </section>
+      <Slant />
+    </>
+  )
+}
+
 export const IndexPage = () => {
   const sectionList = [
     {
@@ -28,12 +62,7 @@ export const IndexPage = () => {
       <Splash />
       <Slant />
       {sectionList.map(section => (
-        <section
-          key={section.title}
-          css={{ textAlign: 'center', height: '100vh' }}
-        >
-          <h1>{section.title}</h1>
-        </section>
+        <Section section={section} />
       ))}
     </Layout>
   )
