973ccfbbf44754b391e08c80ad19de61b071d5d4
Younes Jaaidi <yjaaidi@gmail.com>
2019-11-25 15:39:50 +0100

    🚧 add your coach section

--- src/components/landing/coach-picture.tsx
+++ src/components/landing/coach-picture.tsx
@@ -1,3 +1,4 @@
+import '@emotion/core'
 import { graphql, useStaticQuery } from 'gatsby'
 import GatsbyImage from 'gatsby-image'
 
@@ -13,5 +14,20 @@ export const CoachPicture = ({ path }: { path: string }) => {
       }
     }
   `)
-  return <GatsbyImage fluid={data.file.childImageSharp.fluid}></GatsbyImage>
+  return (
+    <GatsbyImage
+      css={{
+        position: 'absolute',
+        boxSizing: 'border-box',
+        top: '-75px',
+        borderStyle: 'solid',
+        borderColor: 'rgba(255,255,255,.5)',
+        borderWidth: '1px',
+        borderRadius: '50%',
+        margin: 'auto',
+        width: '160px',
+      }}
+      fluid={data.file.childImageSharp.fluid}
+    ></GatsbyImage>
+  )
 }
