d93492c58a906b8017144fa1c3ff2ed944aff203
Younes Jaaidi <yjaaidi@gmail.com>
2019-11-25 17:03:52 +0100

    🚧 add get in touch section

--- src/components/landing/contact.tsx
+++ src/components/landing/contact.tsx
@@ -17,7 +17,7 @@ export const ContactSection = () => {
       content: '+33 (0) 4 26 83 61 92',
     },
     {
-      icon: 'house',
+      icon: 'business_center',
       content: (
         <div css={{ textAlign: 'left' }}>
           <div>SIREN: 827 570 490</div>
@@ -32,28 +32,42 @@ export const ContactSection = () => {
       css={{
         display: 'flex',
         flexDirection: 'column',
+        alignItems: 'center',
+        marginTop: '50px',
       }}
     >
-      {itemList.map(item => (
-        <div
-          css={{
-            alignItems: 'center',
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'center',
-          }}
-          key={item.icon}
-        >
-          <i
-            className="material-icons"
-            style={{ color: marmicodeColor, fontSize: '48px' }}
+      <div
+        css={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'start',
+        }}
+      >
+        {itemList.map(item => (
+          <div
+            css={{
+              alignItems: 'center',
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'center',
+              marginTop: '10px',
+            }}
+            key={item.icon}
           >
-            {item.icon}
-          </i>
-          {item.content}
-        </div>
-      ))}
-      <div></div>
+            <i
+              className="material-icons"
+              style={{
+                color: marmicodeColor,
+                fontSize: '36px',
+                marginRight: '10px',
+              }}
+            >
+              {item.icon}
+            </i>
+            {item.content}
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
