ee332b2fca31a609930b2e24f48188e450c000e6
Younes Jaaidi <yjaaidi@gmail.com>
2019-11-25 15:22:52 +0100

    🚧 add your coach section

--- src/components/landing/coach.tsx
+++ src/components/landing/coach.tsx
@@ -1,6 +1,18 @@
 import '@emotion/core'
+import { graphql, useStaticQuery } from 'gatsby'
 
 export const CoachSection = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      markdownRemark(fileAbsolutePath: { glob: "**/content/coach.md" }) {
+        frontmatter {
+          title
+        }
+        html
+      }
+    }
+  `)
+
   return (
     <div
       css={{
@@ -11,7 +23,16 @@ export const CoachSection = () => {
         marginTop: '50px',
       }}
     >
-      test tseasdfa asdf adsf
+      <h3>{data.markdownRemark.frontmatter.title}</h3>
+      <h4>{data.markdownRemark.frontmatter.title}</h4>
+      Web Developer, Trainer & eXtreme Programming Coach Après 10 ans
+      d’expérience en développement web et sécurité, Younes est convaincu que la
+      qualité d’un produit repose principalement sur le Collective Ownership et
+      la qualité de code. Son but est de propager cette culture de partage et
+      d’améliorer la qualité des produits par l’échange et le levier de la
+      Developer eXperience. Il est l’auteur du Guide Angular …et prend parfois
+      le temps d’aller bavarder aux meetups et conférences sur différents sujets
+      tels que Les Composants Interchangeables à l’AngularConnect
     </div>
   )
 }
---
+++ src/content/coach.md
@@ -0,0 +1,10 @@
+---
+name: Younes Jaaidi
+title: Web Developer, Trainer & eXtreme Programming Coach
+---
+
+Après 10 ans d’expérience en développement web et sécurité, Younes est convaincu que la qualité d’un produit repose principalement sur le Collective Ownership et la qualité de code. Son but est de propager cette culture de partage et d’améliorer la qualité des produits par l’échange et le levier de la Developer eXperience.
+
+Il est l’auteur du Guide Angular
+
+…et prend parfois le temps d’aller bavarder aux meetups et conférences sur différents sujets tels que Les Composants Interchangeables à l’AngularConnect
