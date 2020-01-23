845de60d16856374876c3c2db0de26a0173a452e
Younes Jaaidi <yjaaidi@gmail.com>
2020-01-23 13:59:54 +0100

    🚧 add workshop page

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,55 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const path = require('path')
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const workshopTemplate = path.resolve(`src/templates/workshop.tsx`)
+
+  const result = await graphql
+    `
+      query loadPagesQuery($limit: Int!) {
+        allMarkdownRemark(
+        
+        filter: { fileAbsolutePath: { glob: "**/content/workshops/*.md" } }
+        sort: { fields: [fileAbsolutePath] }
+          limit: $limit
+        ) {
+          edges {
+            node {
+              frontmatter {
+#                slug
+                title
+              }
+            }
+          }
+        }
+      }
+    `,
+    { limit: 1000 }
+  )
+
+  if (result.errors) {
+    throw result.errors
+  }
+
+  for (const edge of result.data.allMarkdownRemark.edges) {
+    createPage({
+      path: `${edge.node.frontmatter.slug}`,
+      component: workshopTemplate,
+      context: {
+        // Add optional context data to be inserted
+        // as props into the page component..
+        //
+        // The context data can also be used as
+        // arguments to the page GraphQL query.
+        //
+        // The page "path" is always available as a GraphQL
+        // argument.
+      },
+    })
+  }
+
+}
+
---
+++ src/templates/workshop.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const WorkshopPage = () => (
+  <Layout title={'Workshop'}>
+    <h1>Workshop</h1>
+  </Layout>
+)
+
+export default WorkshopPage
