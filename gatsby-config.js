7f3624437d045e516355b6b765a68bbba0ace833
Younes Jaaidi <yjaaidi@gmail.com>
2020-05-01 16:12:57 +0200

    ✅ select language with GATSBY_LANGUAGE

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,8 +30,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Marmicode`,
+        short_name: `marmicode`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -3,22 +3,20 @@ import { initReactI18next } from 'react-i18next'
 import { landingEn } from '../components/landing/landing.en'
 import { landingFr } from '../components/landing/landing.fr'
 
-i18n
-  .use(initReactI18next)
-  .init({
-    preload: ['en', 'fr'],
-    fallbackLng: 'fr',
-    lng: 'fr',
-    ns: ['landing'],
-    interpolation: {
-      escapeValue: false, /* not needed for react as it escapes by default. */
+i18n.use(initReactI18next).init({
+  preload: ['en', 'fr'],
+  fallbackLng: 'en',
+  lng: process.env.GATSBY_LANGUAGE,
+  ns: ['landing'],
+  interpolation: {
+    escapeValue: false /* not needed for react as it escapes by default. */,
+  },
+  resources: {
+    en: {
+      landing: landingEn,
     },
-    resources: {
-      en: {
-        landing: landingEn,
-      },
-      fr: {
-        landing: landingFr
-      }
-    }
-  });
+    fr: {
+      landing: landingFr,
+    },
+  },
+})
