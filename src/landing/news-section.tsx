7286a2b668c2b599c9a27bde672dafa56fdf8e8a
manonvarnieu <manon@marmicode.io>
2020-05-27 10:19:00 +0200

    🚧 modify newsletter section & buttons size landing page

--- src/landing/news-section.tsx
+++ src/landing/news-section.tsx
@@ -3,31 +3,65 @@ import TextField from '@material-ui/core/TextField'
 import React from 'react'
 import { theme } from '../config/theme'
 import { LandingSection } from './landing-section'
+import { css } from '@emotion/core'
 
 export function NewsSection() {
   return (
     <LandingSection title={'Subscribe to our newsletter'}>
       <div css={{ display: 'flex', flexDirection: 'column', margin: '10px' }}>
-        <form autoComplete="off">
-          <TextField
-            id="outlined-basic"
-            label="E-mail"
+        <TextField
+          id="outlined-basic"
+          label="Name"
+          variant="outlined"
+          css={{
+            width: '100%',
+            marginBottom: '15px',
+            [theme.breakpoints.up('md')]: {
+              width: '600px',
+            },
+          }}
+        />
+        <div
+          css={css({
+            display: 'flex',
+            flexDirection: 'column',
+            [theme.breakpoints.up('md')]: {
+              flexDirection: 'row',
+            },
+          })}
+        >
+          <form autoComplete="off">
+            <TextField
+              id="outlined-basic"
+              label="E-mail"
+              variant="outlined"
+              css={{
+                width: '100%',
+                [theme.breakpoints.up('md')]: {
+                  width: '600px',
+                },
+              }}
+            />
+          </form>
+          <Button
             variant="outlined"
+            color="primary"
             css={{
-              width: '100%',
+              width: '120px',
+              marginTop: '10px',
               [theme.breakpoints.up('md')]: {
-                width: '600px',
+                marginLeft: '10px',
+                marginTop: '0px',
               },
             }}
-          />
-        </form>
+          >
+            Confirm
+          </Button>
+        </div>
         <p css={{ fontSize: '14px' }}>
           *En renseignant votre adresse mail vous affirmez avoir pris
           connaissance de la politique de confidentialité
         </p>
-        <Button variant="outlined" color="primary" css={{ width: '120px' }}>
-          Confirm
-        </Button>
       </div>
     </LandingSection>
   )
